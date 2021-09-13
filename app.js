20ceb270408ed0e39d42a0e1e048d34a580eaf36
Alvin156 <75882870+Alvin156@users.noreply.github.com>
2021-09-13 14:43:59 +0400

    2. Add todo with buttons (no styling yet)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const todoButton = document.querySelector('.todo-button');
 const todoInput = document.querySelector('.todo-input');
-const todoContainer = document.querySelector('.todo-container');
+const todoList = document.querySelector('.todo-list');
 
 todoButton.addEventListener('click', handleAdd);
 
 /**
-* 
+* ITS FOR INTELLISENSE SHUT
 * @param {PointerEvent} e 
 */
 
@@ -13,7 +13,22 @@ function handleAdd(e) {
     e.preventDefault();
     const todos = document.createElement('div');
     if (!todoInput.value || todoInput.value === '') return;
-    todos.innerText = todoInput.value;
-    todoContainer.appendChild(todos);
+    
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    todos.appendChild(newTodo);
+
+    const checkBtn = document.createElement('button');
+    checkBtn.classList.add('check-button');
+    checkBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+    todos.appendChild(checkBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('trash-button');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    todos.appendChild(deleteBtn);
+
+    todos.classList.add('todo')
+    todoList.appendChild(todos);
     todoInput.value = '';
 }
